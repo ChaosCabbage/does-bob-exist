10ed8b906e54525eb60af38a60afcf63bd1d429b
Paul Courts <puke_4@hotmail.com>
2016-08-02 20:23:46 +0100

    First commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const fs = require("fs")
+const bobImg = document.getElementById("doesBobExist");
+
+function checkForBob() {
+    fs.access("bob.txt", fs.F_OK, function (err) {
+        const image = err ? "no.png" : "yes.png"; 
+        bobImg.src = image;
+    });
+};
+
+setInterval(checkForBob, 1000);
+
